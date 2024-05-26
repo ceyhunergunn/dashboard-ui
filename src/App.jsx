2e9b522f9152035b5ff7b17f22e846cd4f2a9093
ceyhunergunn <ceyhunergun61@gmail.com>
2024-05-26 03:57:22 +0300

    loader animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import Overview from "./components/Overview";
-import { MainContext } from "./context/Context";
+
 import "./globals.css";
+
+import { MainContext } from "./context/Context";
+import Overview from "./components/Overview";
 import Teams from "./components/Teams";
 import TopEmployees from "./components/TopEmployees";
 import Courses from "./components/Courses";
 import ActivityHours from "./components/ActivityHours";
 import TopSkills from "./components/TopSkills";
 import SkillsDevelopment from "./components/SkillsDevelopment";
+import Loader from "./components/Loader";
 
 function App() {
   const { loader } = React.useContext(MainContext);
   return loader ? (
-    <div>asdf</div>
+    <Loader />
   ) : (
     <div className="min-h-screen">
       <Overview />
---
+++ src/components/Loader.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="flex-center min-h-screen">
+      <div class="loader">
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+        <div class="loader-square"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
