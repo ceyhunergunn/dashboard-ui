0d9274b0edde4f039f4eead761de73020302e078
ceyhunergunn <ceyhunergun61@gmail.com>
2024-05-26 06:08:52 +0300

    icons' color changed

--- src/components/ActivityHours.jsx
+++ src/components/ActivityHours.jsx
@@ -152,7 +152,7 @@ const ActivityHours = () => {
             height="30px"
             viewBox="0 -960 960 960"
             width="30px"
-            fill="#fff"
+            fill="#448997"
             className="me-2"
           >
             <path d="M120-120v-80l80-80v160h-80Zm160 0v-240l80-80v320h-80Zm160 0v-320l80 81v239h-80Zm160 0v-239l80-80v319h-80Zm160 0v-400l80-80v480h-80ZM120-327v-113l280-280 160 160 280-280v113L560-447 400-607 120-327Z" />
